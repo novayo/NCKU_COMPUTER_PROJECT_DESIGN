aae5ca90881ca5fe3deb5eaa517cc3a9a1e30f55
novayo <f14051172@gs.ncku.edu.tw>
2019-07-07 22:49:17 +0800

    Beta-1.3.3

--- _firstRunGeth.sh
+++ _firstRunGeth.sh
@@ -1,4 +1,4 @@
-rm -rf ../db
-bash -c 'geth account new --password <(echo 1) --datadir="../db" > .newAccountLog.txt' # https://unix.stackexchange.com/questions/151911/syntax-error-near-unexpected-token
+rm -rf db
+bash -c 'geth account new --password <(echo 1) --datadir="db" > .newAccountLog.txt' # https://unix.stackexchange.com/questions/151911/syntax-error-near-unexpected-token
 node addDefaultAccount_In_Gensis.js
-geth --datadir "../db" init "gensis.json"
+geth --datadir "db" init "gensis.json"
